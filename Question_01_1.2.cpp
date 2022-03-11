91528e9245e714e8df598a114bd2da047a7f8512
Kartekay Kaushik <101121990+SenGoku007@users.noreply.github.com>
2022-03-11 11:50:53 +0530

    Add files via upload

--- Question_01_1.2.cpp
+++ Question_01_1.2.cpp
@@ -7,7 +7,7 @@ public:
     void transpose(vector<vector<int>> matrix)
     {
 
-        for (int i = 0; i < matrix.size(); i++)
+        for (int i = 1; i < matrix.size(); i++)
         {
             for (int j = 0; j < matrix[i].size(); j++)
             {
